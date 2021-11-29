60eb176dbe2d43fc977e8399d53181c0e0315d18
Ranjan-Adarsh <f20190443@pilani.bit-pilani.ac.in>
2021-11-29 16:26:08 +0530

    Some integration

--- src/Disco.c
+++ src/Disco.c
@@ -14,134 +14,13 @@
 #include <string.h>
 #include<stdbool.h>
 
+const int total_websites_limit = 1e3, name_length_of_each_website_limit = 1e2;
 //global variables
 int** mat;
 char** sites;
 int length;
 
-void mainMenuOptions()
-{
-	printf("1. Does every website has a link to itself?\n");
-	printf("2. Is it always possible to return back to the previous website from the current website in one step?\n");
-	printf("3. Does every website has all the links to the websites reachable from it?");
-	printf("4. Does there exists any website that contains a link to itself?");
-	printf("5. Is it impossible to return to the previous website from the current website in on step?\n");
-	printf("6. Is it impossible to return to the previous website from the current website in on step(excluding the cases where the previous and current website is same)?\n");
-	printf("7. Is it possible to divide the network into multiple pieces such that every website in a piece is reachable from every other website in that piece?\n");
-	printf("8. Is this relation an example of poset?\n");
-	printf("9. Exit");
 
-}
-void menu2(int option)
-{
-	printf("Do you want to visualise how the network will look if we add minimum links to satisfy the property?");
-	switch(option)
-	{
-		case 1:
-		case 2:
-		case 3:
-		case 7:
-		default:
-		{
-			printf("Invalid Choice");
-		}
-	}
-}
-void menu3(int option)
-{
-	printf("Do you want to know the nodes in each piece?");
-}
-void menu4Options()
-{
-	printf("1. Display the hasse diagram\n");
-	printf("2. Display the website whose link is availbe in every website\n");
-	printf("3. Display the website which has links to every website\n");
-	printf("4. Display the website that do not have links to any other websites except itself\n");
-	printf("5. Display the websites that can't be reached from any other website except itself\n");
-	printf("6. Given some websites display the websites which are reachable from all of them \n");
-	printf("7. Given some websites, display the websites from which you can reach all those websites\n");
-	printf("8. Is this relation an example of lattice?\n\n");
-	printf("9. Return to Main Menu\n");
-	int cM4;
-	scanf("%d",&cM4);
-	switch(cM4){
-		case 1:
-		{
-			makeHasseMatrix();
-			break;
-		}
-		case 2:
-		{
-			 for (int i = 0; i < length; i++) {
-         bool flag = true;
-         for (int j = 0; j < length; j++) {
-            flag &= (mat[j][i]);
-         }
-         if (flag) {
-            printf("%s\n", sites[i]);
-         }
-      }
-			break;
-		}
-		case 3:{
-			for (int i = 0; i < length; i++) {
-         bool flag = true;
-         for (int j = 0; j < length; j++) {
-            flag &= (mat[i][j]);
-         }
-         if (flag) {
-            printf("%s\n", sites[i]);
-         }
-      }
-			break;
-		}
-		case 4:{
-			for (int i = 0; i < length; i++) {
-         bool flag = true;
-         for (int j = 0; j < length; j++) {
-         	if (i == j) continue;
-            flag &= (!mat[i][j]);
-         }
-         if (flag) {
-            printf("%s\n", sites[i]);
-         }
-      }
-			break;
-		}
-		case 5:{
-			for (int i = 0; i < length; i++) {
-         bool flag = true;
-         for (int j = 0; j < length; j++) {
-         	if (i == j) continue;
-            flag &= (!mat[j][i]);
-         }
-         if (flag) {
-            printf("%s\n", sites[i]);
-         }
-      }
-			break;
-		}
-		case 6:{
-			break;
-		}
-		case 7:{
-			break;
-		}
-		case 8:{
-			break;
-		}
-		case 9:{
-			break;
-		}
-	}
-}
-void menu5Options()
-{
-	printf("1. Given 2 websites A and B, display the website which is reachable from both A and B and can also reach all such websites that A nad B can reach.\n");
-	printf("2. Given 2 websites A and B display the website which can reach to both A and B and is also reachable from all such websites which can reach to both A and B\n");
-	printf("3. Is the lattice distributive?\n");
-	printf("4. Return to Menu 4\n");
-}
 
 bool isReflexive()
 {
@@ -195,7 +74,7 @@ bool isAntiSymmetric()
 		{
 			if(i==j)
 				continue;
-			else if(mat[i][j]==mat[j][i])
+			else if(mat[i][j]==mat[j][i] && mat[i][j]==1)
 				return false;
 		}
 	}
@@ -203,7 +82,7 @@ bool isAntiSymmetric()
 }
 bool isPoset()
 {
-	if(isAntiSymmetric() && isReflexive() && isTransitive())
+	if(isAntiSymmetric()==true && isReflexive()==true && isTransitive()==true)
 	{
 		return true;
 	}
@@ -275,7 +154,7 @@ void makeSymmetric()
 		for(int j=0;j<=i;j++)
 		{
 			if(i==j)
-				temp[i][j]=1;
+				continue;
 			else if(mat[i][j]==1 || mat[j][i]==1)
 			{
 				temp[i][j]=1;
@@ -346,32 +225,297 @@ void makeHasseMatrix()
 		}
 	}
 }
-	//Call necesaary funcyion to display hasse matrix
-   
+//Call necesaary funcyion to display hasse matrix
 
 
+void import()
+{
+	FILE *stream = fopen("SampleInput.csv", "r");
+    char line[1024];  //buffer
+    char labels[total_websites_limit][name_length_of_each_website_limit]; 
+    int label_count = 0;
+    /* reading and counting labels/names of the websites */
+    if (fgets(line, sizeof(line), stream)) {
+      int i = 0;
+      char *token;
+      token = strtok(line, ",");
+      while (token != NULL) {
+         strcpy(labels[i], token);
+         token = strtok(NULL, ",");
+         i++;
+         label_count++;
+      }
+    }
+    /* filling the graph as a 2d-matrix */
+    int graph[label_count][label_count];
+    int row = 0;
+    while (fgets(line, sizeof(line), stream)) {
+       char* tmp = strdup(line);
+       char *token;
+       token = strtok(tmp, ",");
+       int column = 0;
+       while (token != NULL) {
+          if (column != 0) {
+             char c[100];
+             strcpy(c, token);
+             graph[row][column - 1] = (c[0] - '0');
+          }   
+          token = strtok(NULL, ",");
+          column++;
+        }
+      free(tmp);
+      row++;
+   }
+   fclose(stream);
+   for (int i = 0; i < label_count; i++) {
+      printf("%s\n", labels[i]);
+	 //sites[i]=labels[i];
+   }
+      for(int i=0;i<length;i++)
+   {
+	   for(int j=0;j<length;j++)
+	   {
+		   printf("%d ",graph[i][j]);
+	   }
+   }
+   length=label_count;
+   for(int i=0;i<length;i++)
+   {
+	   for(int j=0;j<length;j++)
+	   {
+		   mat[i][j]=graph[i][j];
+	   }
+   }
+	//Data retrival ends here
+}
 
-int main(void) {
 
-	//File *f=fopen("","r")
-	  //Data can be easily cleaned
-	int mat1[5][5]={{0,1,0,1,1},{0,0,1,0,1},{0,1,0,1,1},{1,0,1,0,1},{1,1,1,1,1}};
-	mat= (int **)mat1[0];
-	char sites1[5][40]={"TheTreeCut.com","ConveyLaws.com","ZerosMind.com","RisingMind.com","WaveWaters.com"};
-	sites= (char **)sites1[0];
-	length=5;
-	 int choice;
-	 printf("\n\t\t\tMain menu\n");
-	 mainMenuOptions();
-	 scanf("%d",&choice);
+//LEARN ABOUT THE EXPORT FUNCTION   
+void export (char *filename, int** A) {  
+   filename = strcat(filename, ".csv");
+   FILE *fp = fopen(filename, "w+");
+   for (int i = 0; i < length; i++) {
+      fprintf(fp,",%s", sites[i]);
+   }
+   fprintf(fp,"\n");
+   for (int i = 0; i < length; i++) {
+      fprintf(fp,"%s", sites[i]);
+      for (int j = 0; j < length; j++) {
+         fprintf(fp,",%d", A[i][j]);
+      }    
+      fprintf(fp,"\n");
+   }
+   fclose(fp);
+}
+
+void menu2(int option)
+{
+	char choice;
+	printf("Do you want to visualise how the network will look if we add minimum links to satisfy the property?Press y for yes and any key for no");
+	scanf("%c",&choice);
+	if(choice=='y' || choice=='Y');
+	{
+	switch(option)
+	{
+		case 1:
+		{
+			makeReflexive();
+			break;
+		}
+		case 2:
+		{
+			makeSymmetric();
+			break;
+		}
+		case 3:
+		{
+			makeTransitive();
+		}
+		case 7:
+		{
+
+		}
+		default:
+		{
+			printf("Invalid Choice");
+		}
+	}
+}
+return;
+}
+
+
+void menu3(int option)
+{
+	printf("Do you want to know the nodes in each piece?");
+}
+void menu4Options()
+{
+	printf("1. Display the hasse diagram\n");
+	printf("2. Display the website whose link is availbe in every website\n");
+	printf("3. Display the website which has links to every website\n");
+	printf("4. Display the website that do not have links to any other websites except itself\n");
+	printf("5. Display the websites that can't be reached from any other website except itself\n");
+	printf("6. Given some websites display the websites which are reachable from all of them \n");
+	printf("7. Given some websites, display the websites from which you can reach all those websites\n");
+	printf("8. Is this relation an example of lattice?\n\n");
+	printf("9. Return to Main Menu\n");
+	int cM4;
+	scanf("%d",&cM4);
+	switch(cM4){
+		case 1:
+		{
+			makeHasseMatrix();
+			break;
+		}
+		case 2:
+		{
+		for (int i = 0; i < length; i++) {
+         bool flag = true;
+         for (int j = 0; j < length; j++) {
+            flag &= (mat[j][i]);
+         }
+         if (flag) {
+            printf("%s\n", sites[i]);
+         }
+      	}
+			break;
+		}
+		case 3:{
+			for (int i = 0; i < length; i++) {
+         bool flag = true;
+         for (int j = 0; j < length; j++) {
+            flag &= (mat[i][j]);
+         }
+         if (flag) {
+            printf("%s\n", sites[i]);
+         }
+      }
+			break;
+		}
+		case 4:{
+			for (int i = 0; i < length; i++) {
+         bool flag = true;
+         for (int j = 0; j < length; j++) {
+         	if (i == j) continue;
+            flag &= (!mat[i][j]);
+         }
+         if (flag) {
+            printf("%s\n", sites[i]);
+         }
+      }
+			break;
+		}
+		case 5:{
+			for (int i = 0; i < length; i++) {
+         bool flag = true;
+         for (int j = 0; j < length; j++) {
+         	if (i == j) continue;
+            flag &= (!mat[j][i]);
+         }
+         if (flag) {
+            printf("%s\n", sites[i]);
+         }
+      }
+			break;
+		}
+		case 6:{
+			break;
+		}
+		case 7:{
+			break;
+		}
+		case 8:{
+			break;
+		}
+		case 9:{
+			break;
+		}
+	}
+}
+void menu5Options()
+{
+	printf("1. Given 2 websites A and B, display the website which is reachable from both A and B and can also reach all such websites that A nad B can reach.\n");
+	printf("2. Given 2 websites A and B display the website which can reach to both A and B and is also reachable from all such websites which can reach to both A and B\n");
+	printf("3. Is the lattice distributive?\n");
+	printf("4. Return to Menu 4\n");
+}
+void mainMenuOptions()
+{
+	int choice;
+	printf("\n\t\t\tMain menu\n");
+	printf("1. Does every website has a link to itself?\n");
+	printf("2. Is it always possible to return back to the previous website from the current website in one step?\n");
+	printf("3. Does every website has all the links to the websites reachable from it?\n");
+	printf("4. Does there exists any website that contains a link to itself?\n");
+	printf("5. Is it impossible to return to the previous website from the current website in on step?\n");
+	printf("6. Is it impossible to return to the previous website from the current website in on step(excluding the cases where the previous and current website is same)?\n");
+	printf("7. Is it possible to divide the network into multiple pieces such that every website in a piece is reachable from every other website in that piece?\n");
+	printf("8. Is this relation an example of poset?\n");
+	printf("9. Exit\n");
+	scanf("%d",&choice);
 	 switch(choice)
 	 {
 	 	case 1:
+		 {
+			 if(isReflexive())
+			 {
+				 printf("Yes");
+			 }
+			 else{
+				 menu2(choice);
+			 }
+		 }
 	 	case 2:
+		 {
+			 if(isSymmetric())
+			 {
+				 printf("Yes");
+			 }
+			 else{
+				 menu2(choice);
+			 }
+		 }
 	 	case 3:
+		 {
+			 if(isTransitive())
+			 {
+				 printf("Yes");
+			 }
+			 else{
+				 menu2(choice);
+			 }
+		 }
 	 	case 4:
+		 {
+			 if(hasAtleastOneReflexive()==true)
+			 {
+				 printf("Yes\n");
+			 }
+			 else{
+				 printf("No\n");
+			 }
+		 }
 	 	case 5:
+		 {
+			  if(hasAtleastOneSymmetric()==true)
+			 {
+				 printf("Yes\n");
+			 }
+			 else{
+				 printf("No\n");
+			 }
+		 }
 	 	case 6:
+		 {
+			  if(hasAtleastOneTransitive()==true)
+			 {
+				 printf("Yes\n");
+			 }
+			 else{
+				 printf("No\n");
+			 }
+		 }
 	 	case 7:
 	 	case 8:
 	 	case 9:
@@ -386,5 +530,19 @@ int main(void) {
 	 	}
 	 }
 
+}
 
+
+
+int main(void) {
+	import();
+	for(int i=0;i<length;i++)
+	{
+		for(int j=0;j<length;j++)
+		{
+			printf("%d",mat[i][j]);
+		}
+	}
+	mainMenuOptions();
+	return 0;
 }
