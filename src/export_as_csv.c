18ae63dffbf2ed2ae2b81c4ab808061db5ef3545
Mayank-Vishnoi <f20190098@pilani.bits-pilani.ac.in>
2021-11-30 16:17:20 +0530

    changes to export_as_csv, test now

--- src/export_as_csv.c
+++ src/export_as_csv.c
@@ -1,18 +1,20 @@
 // label_count = global length variable
+// would use labels, a char array to fill names
+// if this doesn't work then manually create a empty csv file with same filename.csv that you're going to enter as parameter   
 
-void export (char *filename, int A[][label_count]) {
+void export (char *filename, int **A) {
    filename = strcat(filename, ".csv");
    FILE *fp = fopen(filename, "w+");
    for (int i = 0; i < label_count; i++) {
-      fprintf(",%s", labels[i]);
+      fprintf(fp, ",%s", labels[i]);
    }
-   fprintf("\n");
+   fprintf(fp, "\n");
    for (int i = 0; i < label_count; i++) {
-      fprintf("%s", labels[j]);
+      fprintf(fp, "%s", labels[j]);
       for (int j = 0; j < label_count; j++) {
-         fprintf(",%d", A[i][j]);
+         fprintf(fp, ",%d", A[i][j]);
       }    
-      fprintf("\n");
+      fprintf(fp, "\n");
    }
    fclose(fp);
 }
